0591fa6fcfcee18d66c5792a9d1ae66219136a9b
hugo.lewagon@gmail.com <hugo.lewagon@gmail.com>
2024-11-12 14:04:05 +0000

    int_orders_operational

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,15 +1,15 @@
 SELECT 
-    salesmargin.orders_id
-    , salesmargin.date_date
-    , ROUND (salesmargin.margin + ship.shipping_fee - (ship.logcost + ship.ship_cost), 2) AS operational_margin
-    , salesmargin.quantity
-    , salesmargin.revenue
-    , salesmargin.purchase_cost
-    , salesmargin.margin
+    ordersmargin.orders_id
+    , ordersmargin.date_date
+    , ROUND (ordersmargin.margin + ship.shipping_fee - (ship.logcost + ship.ship_cost), 2) AS operational_margin
+    , ordersmargin.quantity
+    , ordersmargin.revenue
+    , ordersmargin.purchase_cost
+    , ordersmargin.margin
     , ship.shipping_fee
     , ship.logcost
     , ship.ship_cost
-FROM {{ ref('int_sales_margin') }} AS salesmargin 
+FROM {{ ref('int_orders_margin') }} AS ordersmargin 
 LEFT JOIN {{ ref('stg_raw__ship') }} AS ship 
 USING (orders_id)
 ORDER BY orders_id DESC
